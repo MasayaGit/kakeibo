71949bcbaa2a36a64e429b0288b92aabb41cc4ef
MasayaGit <pontamasaya@i.softbank.jp>
2019-05-19 12:29:58 +0900

    improve cost

--- src/Controller/ShowCostController.php
+++ src/Controller/ShowCostController.php
@@ -7,10 +7,13 @@ use App\Entity\Person;
 use App\Form\CostType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
 
 class ShowCostController extends AbstractController
 {
@@ -32,7 +35,7 @@ class ShowCostController extends AbstractController
     }
 
     /**
-     * @Route("/showcostByCategory/{id}", name="showcostbyvategory")
+     * @Route("/showcostByCategory/{id}", name="showcostbycategory")
      */
     public function showcostByCategory(Request $request,Person $person)
     {
@@ -66,4 +69,62 @@ class ShowCostController extends AbstractController
             ]);
         }
     }
+
+
+    /**
+     * @Route("/showcostByYear/{id}", name="showcostbyvategory")
+     */
+    public function showcostByYear(Request $request,Person $person)
+    {
+        $cost = new Cost();
+        $form = $this->createFormBuilder()
+            ->add('time', DateType::class,['widget' => 'choice','format'=>'yyyy-MM-dd',])
+            ->add('choice', ChoiceType::class, [
+                'choices' => [
+                    '全て'=> 'all',
+                    '年' => 'year',
+                    '月' => 'month',
+                    '日' => 'day',
+                ]])
+            ->add('save', SubmitType::class, ['label' => 'Click'])
+            ->getForm();;
+        $form->handleRequest($request);
+
+        if ($request->getMethod() == 'POST'){
+            $selectedtime = $form->get('time')->getData();
+            $selectedchoice = $form->get('choice')->getData();
+
+            $repository = $this->getDoctrine()
+                ->getRepository(Cost::class);
+
+            if ($selectedchoice  == 'all'){
+                $result = $repository->findByPerson($person);
+            }
+            if ($selectedchoice  == 'year'){
+                $selectedtime = $selectedtime->format('y');
+                $selectedtime = '20'.$selectedtime;
+                $result = $repository->findByYear($person,$selectedtime);
+            }
+            if ($selectedchoice  == 'month'){
+                $selectedtime = $selectedtime->format('y-m');
+                $selectedtime = '20'.$selectedtime;
+                var_dump($selectedtime);
+                $result = $repository->findByMonth($person,$selectedtime);
+            }
+            if ($selectedchoice  == 'day'){
+                $result = $repository->findByDay($person,$selectedtime);
+            }
+            return $this->render('show_cost/result.html.twig', [
+                'title' => "show_cost",
+                'cost' => $result,
+            ]);
+
+        } else {
+            return $this->render('show_cost/form.html.twig', [
+                'title' => 'show_cost',
+                'form' => $form->createView(),
+            ]);
+        }
+    }
+
 }
